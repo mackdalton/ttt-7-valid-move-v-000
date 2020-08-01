c720488c1816767db2ab67074348ceaf3d27f311
Mack Dalton <mackdalton@gmail.com>
2020-08-01 19:00:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken?
     false
-    elsif index between 
+    elsif index.between 
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
